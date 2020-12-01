472ee232a4a15192d451c4872bbeba255f095256
unknown <alejandrojzeballos@gmail.com>
2020-12-01 01:35:43 -0400

    instalcion mongodb interaccion entre mongod y mongo

--- apuntes/apuntes.js
+++ apuntes/apuntes.js
@@ -11,6 +11,21 @@ Siguen un schema
 Scalado horizontal, mientras vaya creciendo podemos agregar mas carga u otro cluster
 Usa JSON como datos.
 
+instalacion
+https://www.mongodb.com/try/download/community
 
+C:\Program Files\MongoDB\Server\4.4\data\
+C:\Program Files\MongoDB\Server\4.4\bin
 
+agregamos al path
+
+mongod --version para ver la version
+mongod para ejecutar mongo en consola
+
+en otra pantalla de consola ejecutamos
+mongo
+que es el interprete y se conecta a mongod en el puerto 27017
+
+EL intepreta porcesa datos
+2+2
 */
