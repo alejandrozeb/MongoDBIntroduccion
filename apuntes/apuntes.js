6e96cc80234cb52cb3101ef005efce60ddd866f3
unknown <alejandrojzeballos@gmail.com>
2020-12-01 00:52:25 -0400

    caracteristicas de mongodb

--- apuntes/apuntes.js
+++ apuntes/apuntes.js
@@ -1,5 +1,16 @@
 /* 
 ORM de nodejs
 
+No solo SQL  NOSQL
+para aplicaciones que manejan big data 
+es escalable y general mediante cluster.
+ESCRITO EN C++ Y MULTIPLATAFORMA.
+Oreintaddo a documentos
+Es orientado a objetos y cada registro que guardemos se llamra documento. 
+Siguen un schema
+Scalado horizontal, mientras vaya creciendo podemos agregar mas carga u otro cluster
+Usa JSON como datos.
+
+
 
 */
